6f39ae2899a92334fa793defd6f38ba97325753c
kvaps <kvapss@gmail.com>
2019-08-16 02:07:16 +0200

    upd: build scripts

--- dockerfiles/build.sh
+++ dockerfiles/build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
+set -e
 
 cd $(dirname $0)
 
 for image in $(find . -maxdepth 2 -name Dockerfile | awk -F/ '{print $2}'); do
-  export DOCKER_BUILDKIT=1
-  docker build --network=host --build-arg VERSION=${CI_COMMIT_REF_NAME-latest} -t ${CI_REGISTRY_IMAGE:-kvaps}/$image:${CI_COMMIT_REF_NAME:-latest} --progress plain $image
+  (set -x; docker build -t ${CI_REGISTRY_IMAGE:-kvaps}/$image:${CI_COMMIT_REF_NAME:-latest} $image)
 done
