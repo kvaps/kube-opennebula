a3d72e85944bbbb2ad5872bdd38976988ec8e459
kvaps <kvapss@gmail.com>
2020-03-25 12:58:44 +0100

    Fix old leaders removal

--- helm/opennebula/scripts/vip.sh
+++ helm/opennebula/scripts/vip.sh
@@ -1,30 +1,46 @@
 #!/bin/bash
-
-# Workaround: https://github.com/OpenNebula/one/issues/4357
-if [ "$HEM_INTEGRATED" = 1 ]; then
-  case $2 in
-    leader)
-      onehem-server start
-    ;;
-    follower)
-      onehem-server stop
-    ;;
-  esac
-fi
-
 set -e
-echo -n "Setting pod/$HOSTNAME $1=$2 - "
 CA_CERT=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
 TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
-DATA="[{\"op\": \"add\", \"path\": \"/metadata/labels/$1\", \"value\": \"$2\"}]"
 NAMESPACE=$(cat /var/run/secrets/kubernetes.io/serviceaccount/namespace)
-URL="https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/api/v1/namespaces/$NAMESPACE/pods/$HOSTNAME"
-STATUS=$(curl -o /dev/null -w '%{http_code}' -sS -m5 --cacert "$CA_CERT" -H "Authorization: Bearer $TOKEN" --request PATCH --data "$DATA" -H "Content-Type:application/json-patch+json" "$URL")
+API_URL="https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT"
+ROLE_LABEL=${ROLE_LABEL:-role}
+ROLE_LABEL_LEADER=${ROLE_LABEL_LEADER:-leader}
+ROLE_LABEL_FOLLOWER=${ROLE_LABEL_FOLLOWER:-follower}
 
-if [ $STATUS -eq 200 ]; then
-  echo "Success (http_code: $STATUS)"
-  exit 0
-else
-  echo "Fail (http_code: $STATUS)"
-  exit 1
-fi
+pcurl() {
+  curl -f -k -sS --cacert "$CA_CERT" -H "Authorization: Bearer $TOKEN" -H "Accept: application/json" "$@"
+}
+parse_names() {
+  ruby -rjson -e "JSON.parse(STDIN.read)['items'].each {|item| puts item['metadata']['name']}"
+}
+label_pod() {
+    echo "Setting pod/$1 ${2%%=*}=${2##*=}"
+    pcurl -XPATCH -o /dev/null --data "[{\"op\": \"add\", \"path\": \"/metadata/labels/${2%%=*}\", \"value\": \"${2##*=}\"}]" -H "Content-Type:application/json-patch+json" "$API_URL/api/v1/namespaces/$NAMESPACE/pods/$1"
+}
+
+case $1 in
+  leader)
+    # start hem
+    [ "$HEM_INTEGRATED" != 1 ] || onehem-server start || true
+    # set leader
+    label_pod "$HOSTNAME" "$ROLE_LABEL=$ROLE_LABEL_LEADER"
+    # remove old leaders
+    OLD_LEADERS="$(pcurl $CURL_PARAMS -XGET "${API_URL}/api/v1/namespaces/${NAMESPACE}/pods?labelSelector=${ROLE_LABEL}%3D${ROLE_LABEL_LEADER}" | parse_names)"
+    for POD in $OLD_LEADERS; do
+      if [ "$POD" != "$HOSTNAME" ]; then
+        label_pod "$POD" "$ROLE_LABEL=$ROLE_LABEL_FOLLOWER"
+      fi
+    done
+    ;;
+  follower)
+    # stop hem
+    [ "$HEM_INTEGRATED" != 1 ] || onehem-server stop || true
+    # set follower
+    label_pod "$HOSTNAME" "$ROLE_LABEL=$ROLE_LABEL_FOLLOWER"
+    ;;
+  *)
+    echo "Usage: $0 <leader|follower>"
+    exit 1
+    ;;
+esac
